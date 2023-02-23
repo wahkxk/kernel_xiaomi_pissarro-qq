44ba40a909dcf61564c824691466c258329e7874
VoidDev-0 <dev.avax@protonmail.com>
2023-02-23 23:44:12 +0100

    drivers/usb: Don't log usb_state constantly
    - also increase the interval
    
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- drivers/usb/gadget/meta.c
+++ drivers/usb/gadget/meta.c
@@ -1200,7 +1200,7 @@ static struct usb_composite_driver android_usb_driver = {
 #endif
 };
 
-#define USB_STATE_MONITOR_DELAY 3000
+#define USB_STATE_MONITOR_DELAY 30000
 static struct delayed_work android_usb_state_monitor_work;
 static void do_android_usb_state_monitor_work(struct work_struct *work)
 {
